b3317f417102303e6ba9bcb34bcc852678d02706
陈其炜 <chenqw@techsure.com.cn>
2022-03-04 16:26:31 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

---
+++ src/main/java/codedriver/framework/report/dto/ReportStatementVo.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.report.dto;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.EntityField;
+import codedriver.framework.util.SnowflakeUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONField;
+import org.apache.commons.lang3.StringUtils;
+
+public class ReportStatementVo {
+    @EntityField(name = "id", type = ApiParamType.LONG)
+    private Long id;
+    @EntityField(name = "名称", type = ApiParamType.STRING)
+    private String name;
+    @EntityField(name = "是否激活", type = ApiParamType.INTEGER)
+    private Integer isActive;
+    @JSONField(serialize = false)
+    private String configStr;
+    @EntityField(name = "配置", type = ApiParamType.JSONOBJECT)
+    private JSONObject config;
+
+    public Long getId() {
+        if (id == null) {
+            id = SnowflakeUtil.uniqueLong();
+        }
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Integer isActive) {
+        this.isActive = isActive;
+    }
+
+    public String getConfigStr() {
+        if (StringUtils.isBlank(configStr) && config != null) {
+            configStr = config.toString();
+        }
+        return configStr;
+    }
+
+    public void setConfigStr(String configStr) {
+        this.configStr = configStr;
+    }
+
+    public JSONObject getConfig() {
+        if (config == null && StringUtils.isNotBlank(configStr)) {
+            try {
+                config = JSONObject.parseObject(configStr);
+            } catch (Exception ignored) {
+
+            }
+        }
+        return config;
+    }
+
+    public void setConfig(JSONObject config) {
+        this.config = config;
+    }
+}
