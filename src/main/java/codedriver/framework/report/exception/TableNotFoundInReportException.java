6bd4aa873039b2df92975a620cc513afd38921ff
赖文韬 <laiwt@techsure.com.cn>
2022-06-14 17:15:10 +0800

    --bug=1009756 --user=赖文韬 报表下载失败 https://www.tapd.cn/54247054/s/1234999

---
+++ src/main/java/codedriver/framework/report/exception/TableNotFoundInReportException.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.report.exception;
+
+import codedriver.framework.exception.core.ApiRuntimeException;
+
+public class TableNotFoundInReportException extends ApiRuntimeException {
+
+    private static final long serialVersionUID = -6208469224750381783L;
+
+    public TableNotFoundInReportException() {
+        super("没有在报表中找到表格，请检查报表模版内容配置的格式是否正确");
+    }
+}
